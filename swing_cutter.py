8b8ca584b1e2d622387e7ff9e0cdb4b621f774b9
igor <morigo.2015@gmail.com>
2021-07-30 02:12:02 +0300

    работает. сбивается только при существенном изменении освещения с момента установки старт_зоны

--- swing_cutter.py
+++ swing_cutter.py
@@ -25,9 +25,6 @@ class FrameProcessor:
         self.start_zone: StartZone = StartZone(win_name, need_load=False)
 
     def process_frame(self, frame: np.ndarray, frame_cnt: int, zone_draw_mode: bool = False) -> np.ndarray:
-        # if frame_cnt == 1:
-        #     pass  # OnePointZone.reset_zone_point()  # it points to ball so we have to re-init it each time
-
         if FrameProcessor.INPUT_SCALE != 1.0:
             cv.resize(frame, None, fx=FrameProcessor.INPUT_SCALE, fy=FrameProcessor.INPUT_SCALE)  # !!!
         if FrameProcessor.NEED_TRANSPOSE:
@@ -146,18 +143,10 @@ class StartZone:
         self.ball_roi = ROI(frame.shape, self.click_xy, self.MAX_BALL_SIZE * 5)
         self.ball_roi_img = self.ball_roi.extract_roi(frame)
 
-        # prepare image of ball_roi: bgr->gray->blur->open->close
-        # gray = cv.cvtColor(self.ball_roi_img, cv.COLOR_BGR2GRAY)
-        # gray = cv.GaussianBlur(gray, (self.BLUR_LEVEL, self.BLUR_LEVEL), 0)
-        # kernel = np.ones((self.BLUR_LEVEL, self.BLUR_LEVEL), np.uint8)
-        # gray = cv.morphologyEx(gray, cv.MORPH_OPEN, kernel)
-        # gray = cv.morphologyEx(gray, cv.MORPH_CLOSE, kernel)
-
         gray = self.preprocess_image(self.ball_roi_img)
         # Util.show_img(gray, "StartArea: gray", 1)
         # brightest_xy = self.get_closest_brightest(gray)  # find brightest point near click_xy
 
-        # StartArea.thresh_val, thresh_img = cv.threshold(gray, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)
         thresh = self.get_best_threshold(gray)  # , brightest_xy)
         if not thresh:  # can't found ball contour
             return False
@@ -174,7 +163,6 @@ class StartZone:
             if d >= 2 * StartZone.MAX_BALL_SIZE - 4:  # nothing worth is found, contour is for total ball_roi image
                 return False
 
-            # found ball contour: include click_xy, not as big as total ball_roi image
             self.ball_contour = cont_lst[0]
             self.ball_area = cv.contourArea(self.ball_contour)
             zone_center_point = self.xy_zone_2_frame((int(x + d / 2), int(y + d / 2)))
@@ -226,14 +214,9 @@ class StartZone:
 
     def get_current_state(self, frame) -> str:
         # analyze current state of StartArea: 'E' - empty, 'B' - ball, 'M' - mess
-
         # roi = frame[self.zone_y:self.zone_y + self.zone_h, self.zone_x:self.zone_x + self.zone_w]
         roi_img = self.zone_roi.extract_roi(frame)
-        # gray = cv.cvtColor(roi_img, cv.COLOR_BGR2GRAY)
-        # gray = cv.GaussianBlur(gray, (self.BLUR_LEVEL, self.BLUR_LEVEL), 0)
-        # kernel = np.ones((self.BLUR_LEVEL, self.BLUR_LEVEL), np.uint8)
-        # gray = cv.morphologyEx(gray, cv.MORPH_OPEN, kernel)
-        # gray = cv.morphologyEx(gray, cv.MORPH_CLOSE, kernel)
+
         gray = self.preprocess_image(roi_img)
         Util.show_img(gray, "Stream:   gray", 1)
 
@@ -276,7 +259,6 @@ class StartZone:
         cv.rectangle(frame,
                      (self.zone_roi.x, self.zone_roi.y), (self.zone_roi.x + self.zone_roi.w, self.zone_roi.y + self.zone_roi.h), (255, 0, 0), 1)
         cv.drawMarker(frame, self.click_xy, (0, 0, 255), cv.MARKER_CROSS, 20, 1)
-        # cv.drawMarker(frame, self.xy_zone_2_frame(self.closest_brightest), (0, 255, 0), cv.MARKER_CROSS, 20, 1)
         # # cv.drawContours(frame, [StartArea.contour], 0, (0, 0, 255), 3)
         cv.putText(frame, f"{self.zone_state}", (50, 100), cv.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 0), 2)
         return frame
